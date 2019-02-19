16431a200fe612a5fd5abaf3cbe4571ea5ab9c52
Jon Owings <jon.owings@gmail.com>
2019-02-19 09:21:59 -0500

    remove demo

--- demofiles/cleanup.sh
+++
@@ -1,6 +0,0 @@
-echo "Clean some mess up"
-helm delete --purge $(helm list --namespace demo1 -q)
-helm delete --purge $(helm list --namespace demo2 -q)
-helm delete --purge $(helm list --namespace demo3 -q)
-helm delete --purge pso
-echo "all clean"
